8f1f08264302abd6245aad781f84524110b2a070
Daniela Plascencia <daplascen@gmail.com>
2017-02-26 21:18:26 -0600

    Update System1.m

--- System1.m
+++ System1.m
@@ -15,7 +15,7 @@ c = 1.7;
 G = tf(b, [a,c])
 [y,t] = step(G);
 % ------ A ------
-G_A = G*b;
+G_A = G*b
 [y_a,t_a] = step(G_A);
 S_A = stepinfo(G_A);
 disp('System 1-A');
@@ -23,7 +23,7 @@ disp(S_A);
 
 % ------ B -------
 k_b = a;
-G_B = tf((b*k_b), [a,(c+(b*k_b))]);
+G_B = tf((b*k_b*b), [a,(c+(b*k_b))])
 [y_b,t_b] = step(G_B);
 S_B = stepinfo(G_B);
 disp('System 1-B');
@@ -55,7 +55,7 @@ r_e = a + c;
 k_e = double(k_dd);
 num = b*r_e*k_e;
 den = c+(k_e*b);
-G_E = tf(num, [a, den]);
+G_E = tf(num, [a, den])
 [y_e,t_e] = step(G_E);
 S_E = stepinfo(G_E);
 disp('System 1-E');
